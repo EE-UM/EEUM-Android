ce28c9fabd1c28ea1a8c59420308047f9e98b6f4
LeeJaeHyeok97 <hazardous10@naver.com>
2026-01-25 00:59:50 +0900

    feat: share 기능까지 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "com.example.eeum"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.eeum"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
